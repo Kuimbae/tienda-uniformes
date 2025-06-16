7185e697a0619cfb07e6c6cfe4a0e2b0a849dfbb
kuimbae <delgadofleitas@hotmail.com>
2025-06-16 13:52:05 +0200

    se agrega base a vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/tienda-uniformes/',
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [react()],
   test: {
     environment: 'jsdom',
