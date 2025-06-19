5d04ddc3b7992c141112cfa8453036b638d2e25e
kuimbae <delgadofleitas@hotmail.com>
2025-06-19 09:46:25 +0200

    agregar linea de codigo del script deploy

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -18,6 +18,8 @@ git commit -m "Deploy to GitHub Pages"
 
 # 4. Forzar push a la rama gh-pages (reemplaza USUARIO y REPO por los tuyos)
 # Obtén la URL del repo automáticamente
+# Elimina el remoto origin si ya existe
+(git remote remove origin 2>/dev/null || true)
 git remote add origin $(git -C .. remote get-url origin)
 git branch -M gh-pages
 git push -f origin gh-pages
