bd0c24c1066237f4b8fbb7745af5df7b2df675e7
kuimbae <delgadofleitas@hotmail.com>
2025-06-23 12:11:13 +0200

    agregado de extension auth.js y products.js y eliminacion de seudo test

--- src/services/auth.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import * as auth from './auth'
-
-describe('auth', () => {
-  it('debería estar definido el módulo', () => {
-    expect(auth).toBeDefined()
-  })
-})
